21e406e2f022b2e4c8b287e9a4ed2075edcace25
Roshaan Siddiqui <siddiqui.roshaan@gmail.com>
2023-12-06 01:55:22 -0600

    chore: use wrap + define request/response types

--- tx-signing-service/src/main.rs
+++ tx-signing-service/src/main.rs
@@ -1,32 +1,72 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+use std::convert::Infallible;
+use std::fmt::Debug;
+use warp::Filter;
 
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello NEAR!!!")
+#[derive(Deserialize, Serialize, Debug)]
+struct QuestValidationInfo {
+    account_id: String,
+    quest_id: String,
+    indexer_function: String,
 }
 
-async fn validate(quest_id: web::Path<String>) -> impl Responder {
-    HttpResponse::Ok().body(format!("Validating quest with id: {}", quest_id))
+#[derive(Serialize, Debug)]
+struct ValidationResponse {
+    valid: bool,
+    message: String,
 }
 
-async fn generate_claim_receipt(info: web::Path<(String, String)>) -> impl Responder {
-    // info.0, info.1
-    HttpResponse::Ok().body(format!(
-        "Generating claim receipt for quest id and account id",
-    ))
+#[derive(Serialize, Debug)]
+struct ClaimReceiptResponse {
+    receipt: String,
+    data: QuestValidationInfo,
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(index))
-            .route("/v1/validate/{quest_id}", web::get().to(validate))
-            .route(
-                "/v1/generate-claim-receipt/{quest_id}/{account_id}",
-                web::get().to(generate_claim_receipt),
+async fn validate_quest(info: QuestValidationInfo) -> Result<impl warp::Reply, Infallible> {
+    println!("info: {:?}", info);
+    Ok(warp::reply::json(&ValidationResponse {
+        valid: true,
+        message: "Hello, World!".to_string(),
+    }))
+}
+
+async fn generate_claim_receipt(info: QuestValidationInfo) -> Result<impl warp::Reply, Infallible> {
+    println!("info: {:?}", info);
+    let signed_tx = "example_signed_tx".to_string();
+
+    Ok(warp::reply::json(&ClaimReceiptResponse {
+        receipt: "".to_string(),
+        data: QuestValidationInfo {
+            account_id: "".to_string(),
+            quest_id: "".to_string(),
+            indexer_function: "".to_string(),
+        },
+    }))
+}
+
+#[tokio::main]
+async fn main() {
+    let validate = warp::path!("v1" / "validate" / String / String / String)
+        .map(
+            |account_id, quest_id, indexer_function| QuestValidationInfo {
+                account_id,
+                quest_id,
+                indexer_function,
+            },
+        )
+        .and_then(validate_quest);
+
+    let generate_claim_receipt =
+        warp::path!("v1" / "generate_claim_receipt" / String / String / String)
+            .map(
+                |account_id, quest_id, indexer_function| QuestValidationInfo {
+                    account_id,
+                    quest_id,
+                    indexer_function,
+                },
             )
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+            .and_then(generate_claim_receipt);
+    let routes = validate.or(generate_claim_receipt);
+
+    warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
 }
