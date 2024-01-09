17143ee19c0eb048a6743c1f5af8d57d6cf21748
Roshaan Siddiqui <siddiqui.roshaan@gmail.com>
2024-01-08 21:40:48 -0600

    update: update to James quest card on-chain code

--- questverse-widgets/apps/QuestVerse/widget/components/quest/claim.jsx
+++ questverse-widgets/apps/QuestVerse/widget/components/quest/claim.jsx
@@ -1,19 +1,15 @@
-const accountId = props.accountId ?? context.accountId;
-const questId = props.questId ?? "813740323";
-
-if (!accountId) {
-  return "";
-}
-
-const loading = joinEdge === null || memberEdge === null;
-
 const isAvailable = true;
 const isClaimed = false;
 
-const type = isClaimed ? "disclaim" : "claim";
+const accountId = props.accountId ?? context.accountId;
+const questId = props.questId;
 
-const handleClaim = () => {
-  Social.set({
+if (!accountId || !questId) {
+  return "No account or quest id provided";
+}
+
+const data = {
+  [accountId]: {
     index: {
       quest: JSON.stringify({
         key: questId,
@@ -23,19 +19,43 @@ const handleClaim = () => {
         },
       }),
     },
-  });
+  },
+};
+
+const type = isClaimed ? "disclaim" : "claim";
+
+const claimQuest = () => {
+  const claimArgs = {
+    claim: {
+      account_id: accountId,
+      quest_id: String(questId),
+    },
+    signature: "signed_signature"
+  };
+  const transactions = [
+    {
+      contractName: "social.near",
+      methodName: "set",
+      args: { data },
+    },
+    {
+      contractName: "v0.questverse.near",
+      methodName: "claim_reward_unverified",
+      args: claimArgs,
+    },
+  ];
+  Near.call(transactions);
 };
 
 return (
-  <>
+  <div className="m-3">
     <button
-      disabled={loading}
       className={`btn ${
-        loading || claimed ? "btn-outline-secondary" : "btn-outline-dark"
+        isClaimed ? "btn-outline-secondary" : "btn-outline-dark"
       }`}
-      onClick={handleClaim}
+      onClick={claimQuest}
     >
-      {loading ? "loading" : closed ? "claimed" : "claim"}
+      {isClaimed ? "claimed" : "claim"}
     </button>
-  </>
+  </div>
 );
